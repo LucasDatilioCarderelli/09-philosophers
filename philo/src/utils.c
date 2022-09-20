66bf080a7d7b656b935e95c0f13cbd5e534b2c96
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-09-20 02:48:33 +0200

    add: atoi and is_digit as utils

---
+++ philo/src/utils.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/18 21:04:02 by ldatilio          #+#    #+#             */
+/*   Updated: 2022/09/18 21:14:38 by ldatilio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sig;
+	int	ret;
+
+	i = 0;
+	sig = 1;
+	ret = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' \
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sig *= -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		ret = ret * 10 + (str[i] - '0');
+		i++;
+	}
+	return (ret * sig);
+}
